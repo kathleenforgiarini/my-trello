b519ded4580804bb1ea797befc16f3bfcbadcd63
Kathleen Forgiarini <kfor.silva@gmal.com>
2021-05-19 21:08:02 -0300

    Estruturação inicial de arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <div class="board">
+      <div class="lane"><h2 class="lane-title">Backlog</h2></div>
+      <div class="lane"><h2 class="lane-title">Desenvolvimento</h2></div>
+      <div class="lane"><h2 class="lane-title">Testes</h2></div>
+      <div class="lane"><h2 class="lane-title">Concluídos</h2></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.board{
+  display: flex;
+  justify-content: flex-start;
+  margin: 1.2rem 0.8rem;
+}
+
+.lane{
+  background: var(--color-grey);
+  width: 23rem;
+  height: 30rem;
+  border-radius: 0.8rem;
+  box-shadow: 0 .1rem .2rem 0 rgba(33,33,33,0.1);
+  margin: 0 0.8rem;
 }
 </style>
---
+++ src/initialCards.js
@@ -0,0 +1,14 @@
+export default {
+    backlog: [
+        { id: 1, text: "Primeiro Card" },
+        { id: 2, text: "Segundo Card" },
+        { id: 3, text: "Terceiro Card" },
+    ],
+    dev: [
+        { id: 4, text: "Quarto Card" },
+        { id: 5, text: "Quinto Card" },
+    ],
+    testes: [
+        { id: 6, text: "Sexto Card" },
+    ]
+};
