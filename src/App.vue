0c3ac8177fa92b6b4173a11d0c5f2dd9116c2044
Kathleen Forgiarini <kfor.silva@gmal.com>
2021-05-19 21:23:50 -0300

    adição de components cards

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,53 @@
   <div id="app">
     <Header/>
     <div class="board">
-      <div class="lane"><h2 class="lane-title">Backlog</h2></div>
-      <div class="lane"><h2 class="lane-title">Desenvolvimento</h2></div>
-      <div class="lane"><h2 class="lane-title">Testes</h2></div>
-      <div class="lane"><h2 class="lane-title">Concluídos</h2></div>
+      <div class="lane">
+        <h2 class="lane-title">Backlog</h2>
+        <Card v-for="card in cards.backlog" :key="card.id">
+          {{ card.text }}
+        </Card>
+      </div>
+      <div class="lane">
+        <h2 class="lane-title">Desenvolvimento</h2>
+        <Card v-for="card in cards.dev" :key="card.id">
+          {{ card.text }}
+        </Card>
+      </div>
+      <div class="lane">
+        <h2 class="lane-title">Testes</h2>
+        <Card v-for="card in cards.testes" :key="card.id">
+          {{ card.text }}
+        </Card>
+      </div>
+      <div class="lane">
+        <h2 class="lane-title">Concluídos</h2>
+        <Card v-for="card in cards.concluidos" :key="card.id">
+          {{ card.text }}
+        </Card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Card from './components/Card';
+import initialCards from './initialCards';
+
 export default {
   name: 'App',
   components: {
     Header,
-  }
+    Card,
+  },
+  data: () => ({
+    cards: {
+      backlog: initialCards.backlog,
+      dev: initialCards.dev,
+      testes: initialCards.testes,
+      concluidos: [],
+    }
+  })
 }
 </script>
 
@@ -34,5 +66,11 @@ export default {
   border-radius: 0.8rem;
   box-shadow: 0 .1rem .2rem 0 rgba(33,33,33,0.1);
   margin: 0 0.8rem;
+  padding: 0 0.7rem;
+}
+
+.lane-title{
+  margin-bottom: 0.6rem;
+  padding: 0.8rem 0.5rem;
 }
 </style>
